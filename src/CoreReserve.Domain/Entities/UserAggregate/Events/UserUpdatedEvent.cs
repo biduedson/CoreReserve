3e6959d7f0fcea764c567455462140318e168a89
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-29 21:21:07 -0300

    feat(core): add UserUpdatedEvent

---
+++ src/CoreReserve.Domain/Entities/UserAggregate/Events/UserUpdatedEvent.cs
@@ -0,0 +1,11 @@
+namespace CoreReserve.Domain.Entities.UserAggregate.Events
+{
+    public class UserUpdatedEvent(
+        Guid id,
+        string name,
+        EGender gender,
+        string email,
+        DateTime createdAt
+    ) : UserBaseEvent(id, name, gender, email, createdAt);
+}
+
