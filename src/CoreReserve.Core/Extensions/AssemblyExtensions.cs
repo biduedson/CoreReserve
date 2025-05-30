0b4f25a2fa0930820e997fb677c9816a2e6be3d6
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-29 23:23:48 -0300

    feat(core): add static class to facilitate operations with assemblies.

---
+++ src/CoreReserve.Core/Extensions/AssemblyExtensions.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+
+namespace CoreReserve.Core.Extensions
+{
+    public static class AssemblyExtensions
+    {
+        public static IEnumerable<Type> GetAllTypesOf<TInterface>(this Assembly assembly)
+        {
+            var isAssignableToTInterface = typeof(TInterface).IsAssignableFrom;
+            return [.. assembly
+         .GetTypes()
+         .Where(type => type.IsClass && !type.IsAbstract && !type.IsInterface && isAssignableToTInterface(type))];
+        }
+    }
+}
