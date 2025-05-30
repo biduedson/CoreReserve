4fb4dd21afe560f2621d0dd044339e36bd043d7e
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-30 01:50:36 -0300

    feat(infrastructure):  add database context class for write operations responsible for managing and storing domain entities

---
+++ src/CoreReserve.Infrastructure/Data/Context/WriteDbContext.cs
@@ -0,0 +1,41 @@
+using CoreReserve.Domain.Entities.UserAggregate;
+using CoreReserve.Infrastructure.Data.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreReserve.Infrastructure.Data.Context
+{
+    /// <summary>
+    /// Contexto do banco de dados para operações de escrita.
+    /// Responsável por gerenciar e armazenar entidades do domínio.
+    /// </summary>
+    public class WriteDbContext(DbContextOptions<WriteDbContext> dbOptions)
+        : BaseDbContext<WriteDbContext>(dbOptions)
+    {
+        /// <summary>
+        /// Representa o conjunto de clientes armazenados no banco de dados.
+        /// </summary>
+        public DbSet<User> Users => Set<User>();
+
+        /// <summary>
+        /// Configura o modelo do banco de dados, aplicando as configurações da entidade Customer.
+        /// </summary>
+        /// <param name="modelBuilder">O construtor do modelo.</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+    }
+
+    // -----------------------------------------
+    // 🔹 EXPLICAÇÃO DO CÓDIGO 🔹
+    // -----------------------------------------
+    /*
+    ✅ Classe WriteDbContext → Define o contexto de banco de dados para operações de escrita. 
+    ✅ Herança de BaseDbContext → Reutiliza configurações globais do banco de dados. 
+    ✅ Propriedade Customers → Define um conjunto do Entity Framework Core para manipulação de entidades Customer. 
+    ✅ Método OnModelCreating() → Aplica a configuração da entidade Customer por meio da classe CustomerConfiguration. 
+    ✅ Esse contexto permite a gestão eficiente de clientes, centralizando regras de persistência e garantindo consistência dos dados. 
+    */
+
+}
