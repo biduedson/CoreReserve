4484e862b4500369b7c32fbf1d7a3ad4d2053e5e
Edson Gomes <biduzao.bidu21@gmail.com>
2025-05-29 22:22:59 -0300

    feat(core): add Interface for cache service, allowing storage and retrieval of temporary data

---
+++ src/CoreReserve.Core/SharedKernel/ICacheService.cs
@@ -0,0 +1,9 @@
+namespace CoreReserve.Core.SharedKernel
+{
+    public interface ICacheService
+    {
+        Task<TItem> GetOrCreateAsync<TItem>(string cacheKey, Func<Task<TItem>> factory);
+        Task<IReadOnlyList<TItem>> GetOrCreateAsync<TItem>(string cacheKey, Func<Task<IReadOnlyList<TItem>>> factory);
+        Task RemoveAsync(params string[] cacheKeys);
+    }
+}
