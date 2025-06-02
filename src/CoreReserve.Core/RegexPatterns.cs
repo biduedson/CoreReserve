ca806c0a86dacfa6b3bcf504235ca8e4715af0d0
Edson Gomes <biduzao.bidu21@gmail.com>
2025-06-02 01:28:09 -0300

    feat(core):  add more methods in regexpatterns

--- src/CoreReserve.Core/RegexPatterns.cs
+++ src/CoreReserve.Core/RegexPatterns.cs
@@ -4,12 +4,23 @@ namespace CoreReserve.Core
 {
     public static partial class RegexPatterns
     {
-
         public static readonly Regex EmailIsValid = EmailRegexPatternAttr();
+        public static readonly Regex HasUppercase = ContainsUppercase();
+        public static readonly Regex HasDigit = ContainsDigit();
+        public static readonly Regex HasSpecialCharacter = ContainsSpecialCharacter();
 
         [GeneratedRegex(
-           @"^([0-9a-zA-Z]([+\-_.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,17})$",
-           RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.CultureInvariant)]
+            @"^([0-9a-zA-Z]([+\-_.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,17})$",
+            RegexOptions.IgnoreCase | RegexOptions.Compiled | RegexOptions.CultureInvariant)]
         private static partial Regex EmailRegexPatternAttr();
+
+        [GeneratedRegex(@"[A-Z]", RegexOptions.Compiled)]
+        private static partial Regex ContainsUppercase();
+
+        [GeneratedRegex(@"\d", RegexOptions.Compiled)]
+        private static partial Regex ContainsDigit();
+
+        [GeneratedRegex(@"[\W_]", RegexOptions.Compiled)]
+        private static partial Regex ContainsSpecialCharacter();
     }
 }
