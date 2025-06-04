493c47fda64b72d9fd1d299159c1fcc47aa95012
Edson Gomes <biduzao.bidu21@gmail.com>
2025-06-04 14:03:28 -0300

    Refactor(domain): add list of errors

--- src/CoreReserve.Domain/ValueObjects/Password.cs
+++ src/CoreReserve.Domain/ValueObjects/Password.cs
@@ -15,20 +15,22 @@ namespace CoreReserve.Domain.ValueObjects
 
         public static Result<Password> Create(string newPassword)
         {
+            var errors = new List<string>();
 
             if (string.IsNullOrWhiteSpace(newPassword))
-                return Result<Password>.Error("Password cannot be empty.");
+                errors.AddRange("Password cannot be empty.");
 
             if (!RegexPatterns.HasUppercase.IsMatch(newPassword))
-                return Result<Password>.Error("Password must contain at least one capital letter.");
+                errors.AddRange("Password must contain at least one capital letter.");
 
             if (!RegexPatterns.HasDigit.IsMatch(newPassword))
-                return Result<Password>.Error("Password must contain at least one number.");
+                errors.AddRange("Password must contain at least one number.");
 
             if (!RegexPatterns.HasSpecialCharacter.IsMatch(newPassword))
-                return Result<Password>.Error("Password must contain at least one special character.");
+                errors.AddRange("Password must contain at least one special character.");
 
-            return Result<Password>.Success(new Password(newPassword));
+            return errors.Any() ? Result<Password>.Error(new ErrorList(errors.ToArray()))
+                               : Result<Password>.Success(new Password(newPassword));
         }
         public override string ToString() => NewPassword;
     }
